c10e8e07ec46eeeae31a118fe873c2bf564b62ec
UnrulyMob <72175425+JPM709@users.noreply.github.com>
2021-04-18 23:10:53 -0230

    created main class and some helpers

--- main/canvas.js
+++ main/canvas.js
@@ -5,47 +5,40 @@ var canvas = document.querySelector('canvas');
 var img = new Image();
 img.src = 'data/bgs/bark0.jpg';
 
-
 var music = new Audio();
 music.src = 'data/sounds/slowforest.mp3';
 
+
 canvas.width = window.innerWidth*0.8;
-canvas.height = window.innerHeight*.8;
-canvas.height = window.innerHeight/2+1;
+canvas.height = window.innerHeight*0.8;
 const HALFW = canvas.width/2;
 const HALFH = canvas.height/2;
 
 var c = canvas.getContext('2d');
-c.fillRect(HALFW-50, HALFH-100, 50, 60);
-c.fillRect(HALFW+50, HALFH-100, 50, 60);
-c.beginPath();
-c.moveTo(HALFW-50,HALFH-10);
-c.lineTo(HALFW+100, HALFH+10);
-c.strokeStyle = 'black';
-c.lineWidth = 10;
-c.stroke();
-//c.fillRect(HALFW-50, HALFH, 150, 10);
-c.font = '30px Arial';
-c.fillText('Oops, something went horribly wrong.', HALFW-250, HALFH+50);
+
+function face(){
+    c.fillRect(HALFW-50, HALFH-100, 50, 60);
+    c.fillRect(HALFW+50, HALFH-100, 50, 60);
+    c.beginPath();
+    c.moveTo(HALFW-50,HALFH-10);
+    c.lineTo(HALFW+100, HALFH+10);
+    c.strokeStyle = 'black';
+    c.lineWidth = 10;
+    c.stroke();
+}
+function oops(){
+    face();
+    c.font = '30px Arial';
+    c.fillText('Oops, something went horribly wrong.', HALFW-250, HALFH+50);
+}
+oops();
+function musicFunc(){
+    if (isPlaying(music) == true){
+        music.pause();
+    }
+    else music.play();
+}
+onmousedown = musicFunc();
 
 
-/*
-c.beginPath();
-c.arc(180,180,70,0,Math.PI*2,true);
-c.moveTo(230,180);
-c.arc(180,180,50,0,Math.PI,false);
-c.moveTo(155,150);
-c.arc(150,150,5,0,Math.PI*2,true);
-c.moveTo(215,150);
-c.arc(210,150,5,0,Math.PI*2,true);
-c.fillText("Hello World!", 165, 270);
-c.stroke();
-*/
 
-/*
-c.beginPath();
-c.moveTo(175,50);
-c.lineTo(350, 150);
-c.strokeStyle = 'yellow';
-c.stroke();
-*/
--- main/player/player.js
+++ main/player/player.js
@@ -1,4 +1,10 @@
-var hunger = 40.00;
-var sticks = 0;
-var logs = 0;
-var axe = 100;
+var player = {
+
+//point of control for countdown init
+hunger: 40.00,
+
+//player resources
+sticks: 0,
+logs: 0,
+axe: 100,
+}
